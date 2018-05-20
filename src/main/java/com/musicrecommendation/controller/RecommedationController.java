3876a4063df12ca4263740da4254f7f314e455fd
Fabricio Valliim <fmvallim@gmail.com>
2018-05-20 16:11:33 -0300

    Adjusting api to receive city as parameter

--- src/main/java/com/musicrecommendation/controller/RecommedationController.java
+++ src/main/java/com/musicrecommendation/controller/RecommedationController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/recommendations")
@@ -15,7 +16,9 @@ public class RecommedationController {
     private RecommendationService recommendationService;
 
     @GetMapping
-    public ResponseEntity getRecommendations() {
+    public ResponseEntity getRecommendations(@RequestParam(name = "city", required = false) String city) {
+
+        recommendationService.findRecommendationsByCity(city);
 
         return ResponseEntity.ok().build();
     }
