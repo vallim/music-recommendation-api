03bd3e102dfd58db00728b1a4929d2ed1b2646fb
Fabricio Valliim <fmvallim@gmail.com>
2018-05-20 16:12:18 -0300

    Consuming openweather api to get temperature given a city

--- src/main/java/com/musicrecommendation/MusicRecommendationApi.java
+++ src/main/java/com/musicrecommendation/MusicRecommendationApi.java
@@ -2,11 +2,18 @@ package com.musicrecommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MusicRecommendationApi {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicRecommendationApi.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
--- src/main/java/com/musicrecommendation/service/TemperatureService.java
+++ src/main/java/com/musicrecommendation/service/TemperatureService.java
@@ -1,11 +1,21 @@
 package com.musicrecommendation.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class TemperatureService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public String findTemperatureByCity(String city) {
-        return "20";
+        String url = "https://api.openweathermap.org/data/2.5/weather?units=metric&APPID=7210b0adeff8918aee50bbe9285690dd&q={city}";
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class, city);
+
+        return responseEntity.getBody();
     }
 }
