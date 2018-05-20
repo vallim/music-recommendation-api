0ab349e29218c36c4036a1c14f3304325cc8daf7
Fabricio Valliim <fmvallim@gmail.com>
2018-05-20 17:45:23 -0300

    Adjusting api to receive lat and long parameters

--- src/main/java/com/musicrecommendation/controller/RecommedationController.java
+++ src/main/java/com/musicrecommendation/controller/RecommedationController.java
@@ -3,6 +3,7 @@ package com.musicrecommendation.controller;
 import com.musicrecommendation.service.RecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +17,16 @@ public class RecommedationController {
     private RecommendationService recommendationService;
 
     @GetMapping
-    public ResponseEntity getRecommendations(@RequestParam(name = "city", required = false) String city) {
+    public ResponseEntity getRecommendations(
+        @RequestParam(name = "city", required = false) String city,
+        @RequestParam(name = "lat", required = false) String lat,
+        @RequestParam(name = "long", required = false) String lon) {
 
-        recommendationService.findRecommendationsByCity(city);
+        if (!StringUtils.isEmpty(city)) {
+            recommendationService.findRecommendationsByCity(city);
+        } else if (!StringUtils.isEmpty(lat) && !StringUtils.isEmpty(lat)) {
+            recommendationService.findRecommendationsByLatLong(lat, lon);
+        }
 
         return ResponseEntity.ok().build();
     }
