b51ffb4753ff1a0fa15b6b6c1bf1cfd4045acf5f
Fabricio Valliim <fmvallim@gmail.com>
2018-05-20 23:16:30 -0300

    Adjusting properties prefix

--- src/main/java/com/musicrecommendation/config/MusicTokenApiConfig.java
+++ src/main/java/com/musicrecommendation/config/MusicTokenApiConfig.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "music.token")
+@ConfigurationProperties(prefix = "music.token.api")
 @Getter
 @Setter
 public class MusicTokenApiConfig {
