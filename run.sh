49315805d09599df9af1b444ede208b36ac0ffb3
Fabricio Valliim <fmvallim@gmail.com>
2018-05-23 01:51:36 -0300

    Adding script to stop the application

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ echo "Building the docker image"
 docker build -t vallim/music-recommendation-api .
 
 echo "Starting the application"
-docker run -d -p 8080:8080 vallim/music-recommendation-api
+docker run --name recommendation-app1 -d -p 8080:8080 vallim/music-recommendation-api
 
 success=200
 
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker stop -t 0 recommendation-app1
+docker rm recommendation-app1
