836d2739a261dd48c5035bc58e9d5dc92d749bc6
Fabricio Valliim <fmvallim@gmail.com>
2018-05-23 01:29:28 -0300

    Dockerizing the application

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Generating the artifact"
+./mvnw clean package
+
+echo "Building the docker image"
+docker build -t vallim/music-recommendation-api .
+
+echo "Starting the application"
+docker run -d -p 8080:8080 vallim/music-recommendation-api
+
+success=200
+
+while :
+do
+	response=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8080/health)
+
+	if [ $response -eq $success ];then
+	  break
+	fi
+done
+
+echo "Started the application successfully"
